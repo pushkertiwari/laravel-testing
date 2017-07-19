b0764163f9167d8e1f67f00814db2077cb5e23db
bhavjot <bhavjot@revinfotech.com>
2017-07-19 10:16:48 +0530

    changes

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
-/**/
+
 class ResetPasswordController extends Controller
 {
     /*
@@ -36,4 +36,5 @@ class ResetPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+    /***/
 }
